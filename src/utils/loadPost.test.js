c40512e13973c774cdfdefceea015586004dc556
varunchandwani <varunchandwani@gmail.com>
2021-02-24 17:34:07 +1100

    Common code moved to testServer file
    Common code moved to testServer file

--- src/utils/loadPost.test.js
+++ src/utils/loadPost.test.js
@@ -1,33 +1,5 @@
 import { loadPost } from "./loadPost";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-
-const server = setupServer(
-	rest.get("https://jsonplaceholder.typicode.com/posts/1", (req, res, ctx) => {
-		return res(
-			ctx.status(200),
-			ctx.json({
-				data: {
-					userId: 2,
-					id: 2,
-					title: "Title",
-					body: "Body",
-				},
-			})
-		);
-	}),
-	rest.get("*", (req, res, ctx) => {
-		console.error(`Please add request handler for ${req.url.toString()}`);
-		return res(
-			ctx.status(500),
-			ctx.json({ error: "Please add request handler" })
-		);
-	})
-);
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
+import { server, rest } from "../testServer";
 
 test("Check loadPost api get call with correct data", async () => {
 	const loadPostData = await loadPost(1);
