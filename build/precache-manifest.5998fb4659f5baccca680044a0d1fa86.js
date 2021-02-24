db70f8c97846ac01ecd592fc04c168dd0dbd1c2c
varunchandwani <varunchandwani@gmail.com>
2021-02-24 18:25:35 +1100

    Build files created
    Build files created

---
+++ build/precache-manifest.5998fb4659f5baccca680044a0d1fa86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb7f89f5ea98da73715345cdaea4759",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "929f9587b227a6483115",
+    "url": "/react-blog/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "e77378f439059ec7b502",
+    "url": "/react-blog/static/css/main.c8486e00.chunk.css"
+  },
+  {
+    "revision": "929f9587b227a6483115",
+    "url": "/react-blog/static/js/2.44cdd29f.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-blog/static/js/2.44cdd29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77378f439059ec7b502",
+    "url": "/react-blog/static/js/main.a576549d.chunk.js"
+  },
+  {
+    "revision": "09d14f40ae754cff9d4a",
+    "url": "/react-blog/static/js/runtime-main.6a80ea60.js"
+  }
+]);
