53cb1e5fc2622a16d64e0169bc5611a1d88eef6a
varunchandwani <varunchandwani@gmail.com>
2021-02-24 18:04:12 +1100

    Build updated
    Build updated

---
+++ build/precache-manifest.f2e679c7428a68b3e4b67aa1d35313cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f946a31e572a5d94c2f262e213e143",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "929f9587b227a6483115",
+    "url": "/react-blog/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "56974687bc0b505d8fdd",
+    "url": "/react-blog/static/css/main.3cf61309.chunk.css"
+  },
+  {
+    "revision": "929f9587b227a6483115",
+    "url": "/react-blog/static/js/2.44cdd29f.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-blog/static/js/2.44cdd29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56974687bc0b505d8fdd",
+    "url": "/react-blog/static/js/main.a576549d.chunk.js"
+  },
+  {
+    "revision": "09d14f40ae754cff9d4a",
+    "url": "/react-blog/static/js/runtime-main.6a80ea60.js"
+  }
+]);
